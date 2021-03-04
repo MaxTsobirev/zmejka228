31c4f0581eb1654f21d5d4b0602b56acbd75ef96
Tsobirev Max <maks61ts@gmail.com>
2021-03-04 15:51:48 +0200

    Добавьте файлы проекта.

---
+++ zmejka/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace zmejka
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetBufferSize(75, 20);
+
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
+
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake zmejka = new Snake(p, 4, Direction.RIGHT);
+			zmejka.Draw();
+
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
+
+			while (true)
+			{
+				if (walls.IsHit(zmejka) || zmejka.IsHitTail())
+				{
+					break;
+				}
+				if (zmejka.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					zmejka.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					zmejka.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+		}
+
+
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+			WriteText("И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("Автор: Максим Цыбирев", xOffset + 2, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+
+	}
+}
